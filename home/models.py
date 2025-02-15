94cb623fe60b29240eae685a27c2fa316b6c870e
tedbra <teddadios@gmail.com>
2025-02-15 12:45:28 +0100

    jobs added to the website

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime, date
 
 # Create your models here.
 
@@ -6,6 +7,7 @@ class Job(models.Model):
     title = models.CharField(max_length=200, null=True) 
     location = models.CharField(max_length=200, null=True, blank=True)
     position = models.CharField(max_length=200, null=True, blank=True)
+    introduction = models.TextField(blank=True)
     responsibilities = models.TextField(blank=True)
     Requirements = models.TextField(blank=True)
     Benefits = models.TextField(blank=True)
@@ -15,15 +17,13 @@ class Job(models.Model):
     jobID = models.CharField(max_length=14, null=True, blank=True, verbose_name='Job ID')
     
     def __str__(self):
-        return self.title + ' | ' + str(self.date_added)
+        return self.title + ' | ' + str(self.date_added.strftime("%d-%m-%Y") )
     
     def save(self, *args, **kwargs):
 
-        current_date = datetime.now()
-        if self.jobID:
-            print('ID OF Job Already Exist = ' + self.jobID)
-        else:
-            date_de_merde = str(current_date)
+        #current_date = datetime.now()
+        if self.id:            
+            date_de_merde = str(self.date_added)
             self.jobID = 'SKYLON'+ date_de_merde[2:4] + str(self.id).zfill(4)
             print('********* THE FOREIGN ID IS : ' + str(self.id))
             print("Why is it going here when there is data in payment history")
--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ from .views import HomeView, JobListView, JobDetailView
 urlpatterns = [
     path('', HomeView, name='home'),
     path('jobs', JobListView.as_view(), name='jobs'),
+    path('jobs/<int:pk>', JobDetailView.as_view(), name='job-detail'),
 ]
