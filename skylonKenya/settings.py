74ea77b28f0c6de85990d978f15be613b5a8c36e
tedbra <teddadios@gmail.com>
2025-02-27 21:38:50 +0100

    added SEO references to skylon schools kenya

--- skylonKenya/settings.py
+++ skylonKenya/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hv1cz8js+7b43rf)h)r^e$be800z=kq=o-s9bcu5s10risdyr0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #ALLOWED_HOSTS = ["kenya.skylon.org", "kenya.skylone.org"]
 
